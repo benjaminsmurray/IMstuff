ece09a96b65560d9a825981491608f45b1b555b2
ben <benjaminsmurray@gmail.com>
2013-08-11 21:27:47 +0000

    refactored to text based client

--- IM.py
+++ IM.py
@@ -1,7 +1,17 @@
+#import tkinter
+#Traceback (most recent call last):
+#  File "<pyshell#11>", line 1, in <module>
+#    import tkinter
+#ImportError: No module named tkinter
+
+import sys, Tkinter
+sys.modules['tkinter'] = Tkinter # put the module where python looks first for modules
+#import tkinter # now works!
 
 from tkinter import Tk, Text, BOTH, W, N, E, S, Frame, Button, Label, StringVar, Message,DISABLED, NORMAL, Scrollbar, LEFT, RIGHT, WORD, Entry,END
+
 import time
-import queue
+import Queue
 import threading
 import socket
 import re
@@ -10,116 +20,29 @@ import sys
 
 
 
-class GuiBox(Frame):
-  
-    def __init__(self, parent, queue, socket, name):
-        self.queue=queue
-        self.chat_sock=socket
-        self.name=name
-        Frame.__init__(self, parent)   
-         
-        self.parent = parent
-        
-        self.initUI()
-        
-    def initUI(self):
-        
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-     
-        #Initial Value for Text Box
-        time_start=time.strftime("%d %b %Y %I:%M:%S",time.localtime())
-        first_txt="Conversation Started: "+time_start+"\n"
-        
-        #Setting up ScrollBar
-        scroll=Scrollbar(self)
-        scroll.grid(row=0,column=1,sticky=N+S)
-        
-        #Setting up chatbox
-        self.chatbox = Text(self, bg="white", width=65, height=30,
-                   yscrollcommand=scroll.set, wrap=WORD)
-
-        self.chatbox.insert("insert",first_txt)
-        self.chatbox.grid(row=0,column=0,sticky=N+E+W)
-        self.chatbox.config(state=DISABLED)
-
-        #Bind scrollbar to the chatbox
-        scroll.config(command=self.chatbox.yview)
-        #Create Composition box
-        self.send=Text(self, width=68, height=3, wrap=WORD)
-        self.send.grid(row=1,column=0, columnspan=2,pady=15)
-        self.send.bind('<KeyRelease-Return>', self.__sendmsg)
-        
-        self.pack()           
-
-    def __sendmsg(self, event):
-        message=self.send.get("1.0",END)
-        self.newmsg(message)
-        self.send.delete("1.0",END)
-
-        
-
-    def newmsg(self, message):
-
-        #format the message to print in chatbox
-        pretty_msg=self.name+": "+message
-
-        #encode message for transfer into socket
-        msg_to_send=message.encode()
-        
-        #send message into socket
-        self.chat_sock.send(msg_to_send)
-
-        #print message in own chatbox
-        
-        self.chatbox.config(state=NORMAL)
-        self.chatbox.insert(END,pretty_msg)
-        self.chatbox.config(state=DISABLED)
-        
-           
-    def getIncoming(self):
-        """Handle messages currently in the queue, if any"""
-        while self.queue.qsize():
-            try:
-                msg=self.queue.get(0)
-                self.chatbox.config(state=NORMAL)
-                self.chatbox.insert(END,msg)
-                self.chatbox.config(state=DISABLED)
-            except queue.Empty:
-                pass
-            
 class LaunchApp:
     """Startup the program and open GUI"""
 
-    def __init__(self,root, socket, name, friend_name):
-        self.tkinterloop=root
+    def __init__(self,socket, name, friend_name):
         self.chat_sock=socket
         self.name=name
         self.friend=friend_name
-
-        #Create queue
-        self.queue=queue.Queue()
-
-        #Create GUI Object
-        self.gui=GuiBox(self.tkinterloop, self.queue, self.chat_sock, self.name)
 
         #Launch thread to do I/O on the socket
         self.running=1
         self.sockthread=threading.Thread(target=self.listen)
         self.sockthread.start()
-
-        self.check_queue()
-
-    def check_queue(self):
-        """Check every 200 ms if there is something new in queue."""
-
-        self.gui.getIncoming()
-        if not self.running:
-            #Force exit
-            import sys
-            sys.exit(1)
-        self.tkinterloop.after(200,self.check_queue)
         
+        self.inputThread = threading.Thread(target=self.newmsg())
+        self.inputThread.start()
+        # this is buggy. I dont think inputThread is returning properly so ctrl c causes process to hang
+        # after ctrl c-ing, run ps -C python and manually kill process
+        while True:
+            self.inputThread.join(500)
+            if not self.inputThread.isAlive():
+                print "input thread is dead"
+                sys.exit(1)
+                break
 
     def listen(self):
         """
@@ -130,20 +53,27 @@ class LaunchApp:
             inc_message=self.chat_sock.recv(16384)
             if inc_message!="":
                 pretty_msg=self.friend+": "+inc_message.decode()
-                self.queue.put(pretty_msg)
+                print pretty_msg
+                print (">>"),
+                #that previous print has no newline so stdout doesnt flush, do it manually
+                sys.stdout.flush()
                 inc_message=""
 
-    def endApplication(self):
-        self.running=0
-
-
+    def newmsg(self):
+        while self.running:
+            message = raw_input(">>")
+            
+            #encode message for transfer into socket
+            msg_to_send=message.encode()
         
+            #send message into socket
+            self.chat_sock.send(msg_to_send)
 
 #Options for startup
 def options():
         print("Would you like to:\n 1. Wait for a connection\
         \n 2. Start a connection\n 3. Quit\n")
-        choice=int(input("Input Number "))
+        choice=int(raw_input("Input Number "))
         if [1,2,3].count(choice)==0:
              print("\nInvalid entry, please enter an integer 1, 2, or 3\n")
              time.sleep(.5)
@@ -155,13 +85,11 @@ def options():
 
 
 def main():
-
-
     sendport=12345
     receiveport=12345
     host=socket.gethostname()
 
-    name=input("What is your name? ")
+    name=str(raw_input("What is your name? "))
     print("\nHello "+name+"!!\n")
     time.sleep(.5)
 
@@ -178,8 +106,8 @@ def main():
     #If they selected to initiate
     elif choice==2:
         chat_sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        friend=input("Who do you want to connect to? ")
-        friend_address=input("What is their IP address? ")                  
+        friend=raw_input("Who do you want to connect to? ")
+        friend_address=raw_input("What is their IP address? ")                  
         chat_sock.connect((friend_address,sendport))
         chat_sock.send(name.encode())
 
@@ -189,13 +117,7 @@ def main():
         time.sleep(2)
         sys.exit
 
-    
-    root = Tk()
-    root.title("Windows")
-    root.geometry("700x600+50+50")
-
-    runprogram=LaunchApp(root,chat_sock,name,friend)
-    root.mainloop()  
+    runprogram=LaunchApp(chat_sock,name,friend)
 
 
 if __name__ == '__main__':
